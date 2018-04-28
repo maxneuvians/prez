1f4051c289ce3350fe98348cb0983aef39b9a16b
Max Neuvians <max@neuvians.net>
2018-04-28 12:58:52 -0400

    Value types

--- main.exs
+++ main.exs
@@ -1,11 +1,27 @@
- # People talk a lot about Elixir's functional and concurrent nature.
- # What does that mean?
- #
- # Functional:
- # There is no shared state between functions. What you are doing is 
- # transforming data in a predictable way without side effects. Data is
- # therefore immutable unless it is re-assigned.
- #
- # Concurrent:
- # Short answer: Different things can happen at the same time. ex. Ruby
- # webservers vs. Erlang webserver.
+# Value types
+
+# Integers
+a = 123
+IO.inspect(a)
+
+# Floats
+b = 3.14
+IO.inspect(b)
+
+# Atoms
+c = :pi_atom
+IO.inspect(c)
+
+# Ranges
+d = 1..10
+
+Enum.map(d, fn n -> n end)
+|> IO.inspect
+
+# String
+e = "Hello"
+IO.inspect(e)
+
+# Char lists
+f = 'ABCD'
+IO.inspect(f)
