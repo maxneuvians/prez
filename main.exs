711941c2f0334794be0a016bd052982696966f10
Max Neuvians <max@neuvians.net>
2018-04-28 12:38:52 -0400

    Slide 1

---
+++ main.exs
@@ -0,0 +1,5 @@
+# Elixir is a dynamic and functional language that leverages the
+# Erlang VM. Erlang is usually used in distributed and fault
+# tolerant systems such as telecommunications and highly 
+# scaling web applications and systems such as WhatsApp, AdRoll,
+# Heroku and World of Warcraft.
