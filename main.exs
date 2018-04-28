48506866d75a1bc03075dcd3fa5787827f9068a4
Max Neuvians <max@neuvians.net>
2018-04-28 13:15:28 -0400

    Pattern matching

--- main.exs
+++ main.exs
@@ -1,10 +1,11 @@
 # Pattern matching
 
-# Assigning vs. binding
-# In elixir you are assinging, not binding.
-# "You can think of binding as a label on a suitcase, and assignment as a suitcase."
-#
-x = 1
-f = fn -> x end
-x = 2
-IO.inspect(f.())
+# Magic
+[1, b, 3] = [1, 2, 3]
+IO.inspect(b)
+
+[_, b, 3] = [:crazy_stuff, 2, 3]
+IO.inspect(b)
+
+[_, b, 3] = [1, 2, 4]
+
